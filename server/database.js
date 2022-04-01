6906078bf77cbc5cf98b4790067f8a109367b071
FrankieCamelCase <frankiecamelcase@gmail.com>
2022-04-01 01:48:01 -0500

    Added database.js & .sql

---
+++ server/database.js
@@ -0,0 +1,7 @@
+const Pool = reqiure("pg").Pool;
+
+const pool = new Pool({
+    user: "postgres",
+    password
+    
+})
---
+++ server/database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE learnnpern;
+
+CREATE TABLE todo(
+    id SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+);
